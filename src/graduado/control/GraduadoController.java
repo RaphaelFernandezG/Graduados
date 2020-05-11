3f3a92000f50068b7dce5390e953e0eeb66c7628
RaphaelFernandezG <raphaelandresfg@ufps.edu.co>
2020-05-10 21:23:29 -0500

    prueba api nuevas credenciales correo graduados

--- src/graduado/control/GraduadoController.java
+++ src/graduado/control/GraduadoController.java
@@ -60,10 +60,10 @@ public class GraduadoController extends HttpServlet {
 		
 		
 		
-		String latitud = request.getParameter("latitude");
-		System.out.println("la latitud es" + latitud);	
-		String longitud = request.getParameter("lingitude");
-		System.out.println("la longitud es " + longitud);	
+		String latitude = request.getParameter("latitude");
+		System.out.println("la latitud es" + latitude);	
+		String longitude = request.getParameter("lingitude");
+		System.out.println("la longitud es " + longitude);	
 		
 		Graduado gra = new Graduado();
 		gra.setApellidos(apellidos);
@@ -74,8 +74,8 @@ public class GraduadoController extends HttpServlet {
 		gra.setNombre(nombre);
 		gra.setNumdocum(numdocum);
 		gra.setTelefono(telefono);
-		gra.setLatitud(latitud);
-		gra.setLongitud(longitud);;
+		gra.setLatitud(latitude);
+		gra.setLongitud(longitude);;
 		
 		GraduadoDao graDao = new GraduadoDao();
 		graDao.insert(gra);
